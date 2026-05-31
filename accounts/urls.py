5423a0f6bd2be6ae69609bf6010753842196b43d
Mathew Mbugua <mbuguam443@gmail.com>
2026-06-01 02:21:43 +0300

    Add password change for admin/users

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 from . import views
 
 app_name = 'accounts'
@@ -8,4 +9,11 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
+    path('profile/change-password/', PasswordChangeView.as_view(
+        template_name='accounts/change_password.html',
+        success_url=reverse_lazy('accounts:password_change_done'),
+    ), name='change_password'),
+    path('profile/change-password/done/', PasswordChangeDoneView.as_view(
+        template_name='accounts/change_password_done.html',
+    ), name='password_change_done'),
 ]
